a69372c961397bd3126212177c9e8054f96d0385
Marcelo William <marcelo.willian@live.com>
2026-09-23 14:54:31 -0300

    chore: inicializa repositorio de plugins

---
+++ HotelSphere/Base/LocalPluginContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace HotelSphere.Base
+{
+    public class LocalPluginContext
+    {
+        public IPluginExecutionContext Context { get; }
+        public IOrganizationServiceFactory ServiceFactory { get; }
+        public IOrganizationService Service { get; }
+        public ITracingService TracingService { get; }
+
+        public LocalPluginContext(IServiceProvider serviceProvider)
+        {
+            Context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ServiceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            Service = (IOrganizationService)ServiceFactory.CreateOrganizationService(Context.UserId);
+            TracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+        }
+    }
+}
---
+++ HotelSphere/Base/PluginBase.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace HotelSphere.Base
+{
+    public abstract class PluginBase : IPlugin
+    {
+        private readonly string _pluginClassName;
+
+        protected PluginBase(Type pluginClassType)
+        {
+            _pluginClassName = pluginClassType.ToString();
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            try
+            {
+                var context = new LocalPluginContext(serviceProvider);
+                tracingService.Trace($"Entrando no plugin: {_pluginClassName}");
+
+                ExecutePluginLogic(context);
+            }
+            catch (InvalidPluginExecutionException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"Erro inesperado em {_pluginClassName}: {ex}");
+                throw new InvalidPluginExecutionException(
+                    "Ocorreu um erro ao processar a operação. Contate o suporte.", ex);
+            }
+        }
+
+        protected abstract void ExecutePluginLogic(LocalPluginContext context);
+    }
+
+}
